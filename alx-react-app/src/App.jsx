e729d2cecafa0f584caab74621bdf492be1c2147
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-08-15 22:54:42 +0200

    userprofile component added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -18,6 +19,9 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+    </div>
+    <div>
+      <UserProfile name="Carlos" age="34" bio="Learning Frontend web development at Alx" />
     </div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
