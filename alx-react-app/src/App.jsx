b0b7038f0bc2302140b244c73970511b62f85e95
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-08-15 22:46:02 +0200

    files fixed in their folder

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -11,6 +14,11 @@ function App() {
     <>
       <div>
       <WelcomeMessage />
+      <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
