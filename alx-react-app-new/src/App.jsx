e4dfa0d757549b03291530fbdb797351b9e83646
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-08-18 18:36:11 +0200

    counter file added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -22,6 +23,9 @@ function App() {
     </div>
     <div>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+    <div>
+      <Counter />
     </div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
