cbc7e939f725e57b6588784c6d8e82f1dd9c0b7e
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-08-25 21:35:21 +0200

    link added to Recipelist

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.filteredRecipes);
 
   return (
     <div>
-      {}
+      {recipes.map(recipe => (
+        <Link key={recipe.id} to={`/recipes/${recipe.id}`}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </Link>
+      ))}
     </div>
   );
 };
