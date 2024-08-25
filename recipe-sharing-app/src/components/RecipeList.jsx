1e3ad630090b6f3719b20c4bd5d224d557c0f89a
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-08-25 21:32:57 +0200

    searchbar and recipelist files updated

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,14 @@
+import React from 'react';
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.filteredRecipes);
+  const recipes = useRecipeStore(state => state.filteredRecipes);
 
   return (
     <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      {}
     </div>
   );
 };
+
 export default RecipeList;
