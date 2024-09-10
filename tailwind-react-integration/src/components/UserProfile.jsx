4c0f2662ac28788cd356fd92aca446517abfb9f0
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-09-10 19:24:21 +0200

    hovering effects added

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,20 +1,20 @@
 function UserProfile() {
     return (
-      <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrUfiySJr8Org5W-oE2v3_i7VqufglYtSdqw&s"
           alt="User"
-          className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+          className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
         />
-        <h1 className="text-base sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+        <h1 className="text-base sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
+          John Doe
+        </h1>
         <p className="text-sm sm:text-base text-gray-600">
-          Developer at Example Co. Loves to write code and explore new
-          technologies.
+          Developer at Example Co. Loves to write code and explore new technologies.
         </p>
       </div>
     );
   }
   
   export default UserProfile;
-  
   
