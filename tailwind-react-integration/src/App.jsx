b461577e95b94d6f2b4f8e936aadbb6bb2b17def
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-09-10 18:07:09 +0200

    tailwind css

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="p-4 bg-teal-500 text-white">
+      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
