c33aa6e8589248566d3e3ab1b2911be3b8dacfd7
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-09-10 20:53:34 +0200

    React Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
@@ -11,7 +11,7 @@ import NotFound from "./components/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  
+  // Simulated authentication state
   const isAuthenticated = true;
 
   return (
@@ -20,18 +20,21 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         
-       
-        <Route path="profile" element={<ProtectedRoute isAuthenticated={isAuthenticated} element={<Profile />} />}>
+        {/* Protected Route with Nested Routes */}
+        <Route 
+          path="profile" 
+          element={<ProtectedRoute isAuthenticated={isAuthenticated} element={<Profile />} />}
+        >
           <Route path="details" element={<ProfileDetails />} />
         </Route>
         
-       
+        {/* Dynamic Routing for Blog Posts */}
         <Route path="blog/:id" element={<BlogPost />} />
         
-      
+        {/* Public Route for Login */}
         <Route path="login" element={<Login />} />
         
-       
+        {/* Catch-All Route for 404 Not Found */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
