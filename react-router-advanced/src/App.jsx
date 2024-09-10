a9f6b35c242341f29be695205311ea7e3d230836
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-09-10 20:49:14 +0200

    Routing route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,33 +1,37 @@
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Profile from './components/Profile';
-import ProfileDetails from './components/ProfileDetails';
-import ProfileSettings from './components/ProfileSettings';
-import BlogPost from './components/BlogPost';
-import Login from './components/Login';
-import NotFound from './components/NotFound';
-import ProtectedRoute from './components/ProtectedRoute';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import BlogPost from "./components/BlogPost";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+  
+  const isAuthenticated = true;
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login onLogin={setIsAuthenticated} />} />
-        <Route
-          path="/profile"
-          element={<ProtectedRoute element={<Profile />} isAuthenticated={isAuthenticated} />}
-        >
+        <Route path="about" element={<About />} />
+        
+       
+        <Route path="profile" element={<ProtectedRoute isAuthenticated={isAuthenticated} element={<Profile />} />}>
           <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
         </Route>
-        <Route path="/blog/:postId" element={<BlogPost />} />
+        
+       
+        <Route path="blog/:id" element={<BlogPost />} />
+        
+      
+        <Route path="login" element={<Login />} />
+        
+       
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
