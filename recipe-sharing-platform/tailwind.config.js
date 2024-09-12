46f3c3c839f6b9da0f46939056ade65efd2143f1
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-09-12 18:35:01 +0200

    purde added in tailwind package

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-
